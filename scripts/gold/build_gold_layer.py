919a16ecd307f16821406f2cb4336c194c34b338
Saadmomin2903 <saadmomin5555@gmail.com>
2026-01-25 20:30:42 +0530

    ahdj

--- scripts/gold/build_gold_layer.py
+++ scripts/gold/build_gold_layer.py
@@ -25,8 +25,8 @@ def get_spark_session():
     conf = (
         pyspark.SparkConf()
             .setAppName('gold_layer_builder')
-            # Reverting Cluster Mode due to connection issues.
-            # Fanout Writer below will solve the performance/crashing issue.
+            # FIX 4: Use CLUSTER MODE to utilize VM2 Workers (Optimization)
+            .set("spark.master", "spark://spark-master:7077")
             .set('spark.sql.shuffle.partitions', '200')  # Lower shuffle for aggregations
             .set('spark.jars.packages', 
                  'org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.3.1,'
